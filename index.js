d0abf69147db73e01f06bdb10b482871899a0554
Avram Vlad <vlad@Avrams-MacBook-Pro.local>
2022-06-23 22:08:03 +0300

    emag showcase

--- index.js
+++ index.js
@@ -1,145 +1,69 @@
-// console.log('this is array');
-
-// if(true) {
-//     console.log('this is true')
-// } else {
-//     console.log('this is false')
-// }
-
-// Ternary operator
-
-// let name = 'Vlad';
-
-// name == 'Vlad' ? console.log('numele este vlad') : console.log('numele nu este vlad');
-
-// Array
-
-const fruits = ['mango', 'apple', 1, 2, 3,4, 5, 6, 5, 'orange'];
-
-//Adauga elementul la coada array-ului
-// fruits.push('strawberry');
-
-// fruits.splice(1, 0, 'strawberry')
-// fruits.splice(1, 0, 'strawberry')
-
-// fruits.pop();
-
-// fruits.splice(1, 2);
-
-// fruits.map((fruit, i) => {
-//     if(fruit === 5) {
-//         fruits.splice(i, 1);
-//     }
-// })
-
-
-// console.log(fruits);
-
-const companies = [
-    { name: "Company One", category: "Finance", start: 1981, end: 2003 },
-    { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
-    { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
-    { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
-    { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
-    { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
-    { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
-    { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
-    { name: "Company Nine", category: "Retail", start: 1981, end: 1989 },
-  ];
-
-// let companiesName = [];
-
- for(let i=0; i < companies.length; i++) {
-    // companiesName.push(companies[i].name)
-  }
-
-// let companiesNames = companies.map((company) => {
-//     return company.name
-// })
-
-let companiesTest = companies.map((company) => {
-    const { name, category } = company;
-    return {
-        // name: company.name,
-        // category: company.category
-        name,
-        category
+let productsList = [
+    {
+        title: 'Televizor LED start ',
+        category: 'televizoare',
+        rating: '4.68',
+        price: '499 Lei'
+    },
+    {
+        title: 'Televizor Samsung ',
+        category: 'televizoare',
+        rating: '5',
+        price: '334 Lei'
+    },
+    {
+        title: 'Televizor LED start ',
+        category: 'televizoare',
+        rating: '4.68',
+        price: '499 Lei'
+    },
+    {
+        title: 'Telefon Samung',
+        category: 'telefon',
+        rating: '4.4',
+        price: '500 Lei'
     }
-})
+]
 
-// let calculateYears = companies.map((company) => {
-//     return {
-//         years: `Compania a tinut ${company.end - company.start} ani`
-//     }
-// })
+let container = document.getElementById('container');
 
-// console.log(calculateYears);
+let createEmagProducts = (products) => {
+    container.innerHTML = '';
+    products.forEach((product) => {
+        const { title, rating, price } = product;
 
-// let companiesNames = companies.forEach((company, i) => {
-//     let companyNameEl = document.createElement('div');
-//     companyNameEl.innerHTML = company.name;
-//     document.body.appendChild(companyNameEl);
-
-//     // return company.name
-// })
-
-// let companiesNames = companies.map((company) => {
-//     return company.name
-// })
-
-// let companiesNames = companies.forEach((company) => {
-//     console.log(company.name)
-//     return company.name
-// })
-
-// console.log(companiesNames)
-
-// console.log(companiesNames);
-
-// let financeCompanies = companies.filter((company)=> {
-//     return company.category === "Finance" && company.start > 1990
-// })
-
-// console.log(financeCompanies);
-
-let companiesListElement = document.getElementById('companies-list');
-
-let createListElements = (companiesArrayList) => {
-    companiesListElement.innerHTML = "";
-    companiesArrayList.forEach((company) => {
-        let companyElement = document.createElement('li');
-        companiesListElement.appendChild(companyElement);
-        companyElement.innerHTML = `Name: ${company.name} | Category: ${company.category}`
+        console.log(products[0].title)
+        let productContainer = document.createElement('div');
+        productContainer.className ='product-container' ;
+    
+        container.appendChild(productContainer);
+        let productTitle = document.createElement('h1');
+        productContainer.appendChild(productTitle);
+        productTitle.innerHTML = title;
+    
+        let productRating = document.createElement('h2');
+        productContainer.appendChild(productRating);
+        productRating.innerHTML = `Ratingul este: ${rating}`;
+    
+        let productPrice = document.createElement('h3');
+        productContainer.appendChild(productPrice)
+        productPrice.innerHTML = `Pretul este: ${price}`;
     })
 }
 
-let getFinanceCompanies = () => {
-    let companiesCategory = companies.filter((company) => {
-        return company.category === 'Finance'
+
+let getPhones = () => {
+   let phoneProducts = productsList.filter((product) => {
+        return product.category === "telefon"
     })
-    createListElements(companiesCategory);
+    createEmagProducts(phoneProducts)
 }
 
-
-let getRetailCompanies = () => {
-    let companiesCategory = companies.filter((company) => {
-        return company.category === 'Retail'
+let getTv = () => {
+    let tvProducts = productsList.filter((product) => {
+        return product.category === 'televizoare'
     })
-    createListElements(companiesCategory);
+    createEmagProducts(tvProducts);
 }
 
-let getAutoCompanies = () => {
-    let companiesCategory = companies.filter((company) => {
-        return company.category === 'Auto'
-    })
-    createListElements(companiesCategory);
-}
-
-let getTechnologyCompanies = () => {
-    let companiesCategory = companies.filter((company) => {
-        return company.category === 'Technology'
-    })
-    createListElements(companiesCategory);
-}
-
-createListElements(companies);
+createEmagProducts(productsList);
